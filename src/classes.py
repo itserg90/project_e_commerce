790a2a56b630259419c3d5fc0cb7ea17c3b0675b
Sergei Gorlanov <itserg90@yandex.ru>
2024-02-09 03:23:35 +0300

    version 0.1.1 1) split load_json into load_json and convert_json 2) add method goods in class Category 3) make it private products in class Category and add getter for products 4) make it private price in class Product and add getter and setter for price 5) add method new_product in class Product

--- src/classes.py
+++ src/classes.py
@@ -10,11 +10,22 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
-        self.number_of_categories += 1
-        number = len(set(product.name for product in self.products))
+        Category.number_of_categories += 1
+        number = len(set(product.name for product in self.__products))
         self.number_of_products += number
+
+    @property
+    def goods(self):
+        list_products = []
+        for product in self.__products:
+            list_products.append(f"{product.name}, {int(product.price)} руб. Остаток: {product.quantity} шт.")
+        return list_products
+
+    @property
+    def products(self):
+        return self.__products
 
     def __repr__(self):
         return f"Category({self.name}; {self.description}; {self.products})"
@@ -25,13 +36,45 @@ class Product:
     name: str
     description: str
     price: float
-    count_products: int
+    quantity: int
 
-    def __init__(self, name, description, price, count_products):
+    def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = float(price)
-        self.count_products = int(count_products)
+        self.__price = float(price)
+        self.quantity = int(quantity)
+
+    def new_product(self, name, description, price, quantity):
+        """Возвращает новый объект товара и проверяет совпадение нового объекта товара с текущим"""
+        current_object = Product(name, description, price, quantity)
+
+        if self.name == current_object.name:
+            self.quantity += current_object.quantity
+            if current_object.price > self.price:
+                self.price = current_object.price
+        return current_object
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if price > self.__price:
+            self.__price = price
+        elif price < 0 or price == self.__price:
+            print("Цена введена некорректно")
+        else:
+            while True:
+                user_answer = input("Цена понизилась. Установить эту цену?(y-да, n-нет)")
+                if user_answer == "y":
+                    self.__price = price
+                    break
+                elif user_answer == "n":
+                    print("Цена осталась прежней.")
+                    break
+                else:
+                    print("Пожалуйста, введите корректный ответ: y или n(y-да, n-нет)")
 
     def __repr__(self):
-        return f"Product({self.name}; {self.description}; {self.price}; {self.count_products})"
+        return f"Product({self.name}; {self.description}; {self.price}; {self.quantity})"
--- src/utils.py
+++ src/utils.py
@@ -7,10 +7,14 @@ path_name = "https://www.jsonkeeper.com/b/F7TP"
 
 
 def load_json(filename: str) -> list:
-    """Подгружает файл формата JSON и возвращает список обьектов классов Category и Product"""
+    """Подгружает файл формата JSON"""
+    return requests.get(filename).json()
+
+
+def convert_json(categories: list) -> list:
+    """Возвращает список обьектов классов Category и Product"""
 
     list_of_categories = []
-    categories = requests.get(filename).json()
     for category in categories:
         list_of_products = []
         for product in category["products"]:
@@ -25,3 +29,29 @@ def load_json(filename: str) -> list:
         list_of_categories.append(object_category)
 
     return list_of_categories
+
+
+# # Проверка
+# categories_in_list = load_json(path_name)
+#
+# # на изменение цены
+# for obj in convert_json(categories_in_list):
+#     for prod in obj.products:
+#         print(prod.price)
+#         user_price = float(input())
+#         prod.price = user_price
+#     print(obj)
+#
+# # на добавление нового объекта товара
+# for obj in convert_json(categories_in_list):
+#     flag = False
+#     name_cat, name, description, price, quantity = "Смартфоны", "Iphone 15", "512GB, Gray space", 220_000, 8
+#     if obj.name == name_cat:
+#         for prod in obj.products:
+#             if name == prod.name:
+#                 prod.new_product(name, description, price, quantity)
+#                 flag = True
+#                 break
+#         if not flag:
+#             obj.products.append(classes.Product(name, description, price, quantity))
+#     print(obj)
