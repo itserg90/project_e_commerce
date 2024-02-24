db421d5cbb96d2e201d5933b00514de4ad34eeb1
Sergei Gorlanov <itserg90@yandex.ru>
2024-02-24 04:57:21 +0300

    version 0.1.10 1)change method __init__ in class MixinRepr

--- src/mixins.py
+++ src/mixins.py
@@ -1,6 +1,6 @@
 class MixinRepr:
     def __init__(self):
-        MixinRepr.__repr__(self)
+        print(MixinRepr.__repr__(self))
 
     def __repr__(self):
         return f"{self.__class__.__name__}{tuple(self.__dict__.items())}"
